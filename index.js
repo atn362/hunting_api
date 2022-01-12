2fd465ae645fe86a9d5cea3da1f5b6f99f653f86
Alex Norton <atn362@gmail.com>
2022-01-11 19:03:55 -0600

    another website added

--- index.js
+++ index.js
@@ -46,6 +46,11 @@ const newspapers = [
     address: "https://www.themeateater.com/",
     base: "https://www.themeateater.com",
   },
+  {
+    name: "fieldandstream",
+    address: "https://www.fieldandstream.com/",
+    base: "",
+  },
 ];
 
 const articles = [];
@@ -76,29 +81,34 @@ app.get("/news", (req, res) => {
 });
 
 app.get("/news/:newspaperId", (req, res) => {
- const newspaperId = req.params.newspaperId
+  const newspaperId = req.params.newspaperId;
 
- const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
- const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+  const newspaperAddress = newspapers.filter(
+    (newspaper) => newspaper.name == newspaperId
+  )[0].address;
+  const newspaperBase = newspapers.filter(
+    (newspaper) => newspaper.name == newspaperId
+  )[0].base;
 
+  axios
+    .get(newspaperAddress)
+    .then((response) => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const specificArticles = [];
 
-    axios.get(newspaperAddress)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const specificArticles = []
+      $('a:contains("hunt")', html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr("href");
+        specificArticles.push({
+          title,
+          url: newspaperBase + url,
+          source: newspaperId,
+        });
+      });
+      res.json(specificArticles);
+    })
+    .catch((err) => console.log(err));
+});
 
-            $('a:contains("hunt")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-                specificArticles.push({
-                    title,
-                    url: newspaperBase + url,
-                    source: newspaperId
-                })
-            })
-            res.json(specificArticles)
-        }).catch(err => console.log(err))
-})
-
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
